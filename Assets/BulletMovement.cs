5d67186f78b054e8fc5192c37fe01ab7e808bc45
WorldFam <mirekas123@gmail.com>
2022-03-21 10:06:21 +0100

    commit to pull

--- Assets/BulletMovement.cs
+++ Assets/BulletMovement.cs
@@ -13,13 +13,12 @@ public class BulletMovement : MonoBehaviour
     }
     void Update()
     {
-        transform.Translate(Vector2.up * Time.deltaTime * speed);   
+        transform.Translate(Vector3.up * (Time.deltaTime * speed));   
     }
 
     IEnumerator AutoDestroy(float time)
     {
         yield return new WaitForSeconds(time);
-        
         Destroy(gameObject);
     }
 }
--- Assets/ShootingReyCast.cs
+++ Assets/ShootingReyCast.cs
@@ -1,58 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Numerics;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
-using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
 
 public class ShootingReyCast : MonoBehaviour
 {
-    [SerializeField]
-    Transform firePoint;
-    [SerializeField]
-    GameObject bulletPrefab;
+    [SerializeField] Transform firePoint;
+    [SerializeField] GameObject bulletPrefab;
     public float bulletForce = 20f;
-    public float range = 100f; 
+    public float range = 100f;
     public float timeToDestroy = 3f;
+    public int speed = 100;
+    public float fireRate = 15f;
 
-    // Update is called once per frame
-    void Update ()
+    private float timeToFire = 0f;
+    void Update()
     {
-        if (Input.GetButtonDown("Shoot"))
+        if (Input.GetButton("Shoot") && Time.time >= timeToFire)
         {
-           Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-
-           //Use debug with gizmo
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.up) * 10f, Color.red);
+            timeToFire = Time.time + 1f / fireRate;
+            Shoot();
             
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.up),range);
-            if (hit)
-            {
-                Debug.Log("HIT SOMETING : " + hit.collider.name);
-                hit.transform.GetComponent<SpriteRenderer>().color = Color.red;
-            }
-
-            /*Vector2 hitPosition = hit.point;
-            ShootBullet(hitPosition);*/
         }
+        
     }
 
-    /*void ShootBullet(Vector2 hitPosition)
+    void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
-        
-        LineRenderer line = bullet.GetComponent<LineRenderer>();
-        if (line != null)
-        {
-            line.SetPosition(0,firePoint.position);
-            line.SetPosition(1,hitPosition);
+        //Use debug with gizmo
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.up) * 10f, Color.red);
 
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.up), range);
+        if (hit)
+        {
+            Debug.Log("HIT SOMETING : " + hit.collider.name);
+            hit.transform.GetComponent<SpriteRenderer>().color = Color.red;
         }
 
-        Destroy(bullet, 3f);
-    }*/
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
 }
+ 
