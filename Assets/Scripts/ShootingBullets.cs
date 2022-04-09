778627676f82869dd3f33df33c6f74dab015b07a
EdvinasAndrijauskas <edvinas.andrijauskas2000@gmail.com>
2022-04-09 19:06:46 +0200

    Got back last working master

--- Assets/Scripts/ShootingBullets.cs
+++ Assets/Scripts/ShootingBullets.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class ShootingBullets : MonoBehaviour
 {
-public Transform firePoint;
-public GameObject bulletPrefab;
-public float bulletForce = 20f;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce = 20f;
 
 // Update is called once per frame
-void Update()
-{
-    if (Input.GetButtonDown("Shoot"))
+    void Update()
     {
-        Shoot();
+        if (Input.GetButtonDown("Shoot"))
+        {
+            Shoot();
+        }
     }
-}
-void Shoot()
-{
-    GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-    Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-    rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
-}
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
 }
