d5380f77d694185083b43e4cfb4094b6a01b94e4
Sabina Baghiu <elenasabina01@g,ail.com>
2022-05-11 19:30:41 +0200

    Possible to volume up and down

--- Assets/Scripts/VolumeMenu.cs
+++ Assets/Scripts/VolumeMenu.cs
@@ -4,11 +4,17 @@ using UnityEngine.UI;
 
 public class VolumeMenu : MonoBehaviour
 {
+    [SerializeField] private Slider _slider;
     [SerializeField] private GameObject _pauseMenuScene;
     [SerializeField] private GameObject _weaponMenuScene;
     [SerializeField] private GameObject _levelMenuScene;
     [SerializeField] private GameObject _OptionMenuScene;
 
+    private void Start()
+    {
+        VolumeSlider();
+    }
+    
     // Update is called once per frame
     private void Update()
     {
@@ -20,9 +26,23 @@ public class VolumeMenu : MonoBehaviour
 
     public void BackButton()
     { 
+        PlayerPrefs.SetFloat("VolumeValue", _slider.value);
         _pauseMenuScene.SetActive(true);
         _weaponMenuScene.SetActive(false);
         _levelMenuScene.SetActive(false);
         _OptionMenuScene.SetActive(false);
+        LoadValues();
+    }
+
+    public void VolumeSlider()
+    {
+        _slider.onValueChanged.AddListener(val => AudioManager.instance.ChangeMasterVolume(val));
+    }
+    
+    void LoadValues()
+    {
+        float volume = PlayerPrefs.GetFloat("VolumeValue");
+        _slider.value = volume;
+        AudioListener.volume = volume;
     }
 }
